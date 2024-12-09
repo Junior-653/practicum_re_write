a086f1aa4e317ad6f5c45945167d207348842656
Junior-653 <166173016+Junior-653@users.noreply.github.com>
2024-12-09 12:20:50 +0200

    github 1st commit 2nd commit

--- app/src/main/java/com/example/myapplicationpracticum/MainActivity2.kt
+++ app/src/main/java/com/example/myapplicationpracticum/MainActivity2.kt
@@ -1,7 +1,9 @@
 package com.example.myapplicationpracticum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,13 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
 
+        val Button = findViewById<Button>(R.id.button3)
 
+        // Navigate to the next page
+        Button.setOnClickListener {
+            // Intent to move from MainActivity to NextActivity
+            val intent = Intent(this@MainActivity2, MainActivity3::class.java)
+            startActivity(intent)
+        }
     }
 }
