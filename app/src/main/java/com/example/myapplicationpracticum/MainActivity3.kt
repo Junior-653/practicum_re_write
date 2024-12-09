88886410fea46c5834aaf80d39dc4222228e4977
Junior-653 <166173016+Junior-653@users.noreply.github.com>
2024-12-09 12:49:48 +0200

    github 1st commit 2nd commit 3rd commit final commit

--- app/src/main/java/com/example/myapplicationpracticum/MainActivity3.kt
+++ app/src/main/java/com/example/myapplicationpracticum/MainActivity3.kt
@@ -2,10 +2,18 @@ package com.example.myapplicationpracticum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        val button = findViewById<Button>(R.id.button4)
+        //Exit app
+        button.setOnClickListener {
+            finishAffinity()
+        }
+
     }
 }
